20a89b98925fbd25047aeab543433f8ca8567752
manaswithasunkara <manaswitha.sunkara@outlook.com>
2023-04-22 09:23:51 +0530

    comitted

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -1,7 +1,10 @@
+import 'package:chat_app/widgets/chat_bubble.dart';
+import 'package:chat_app/widgets/chat_input.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatelessWidget {
   const ChatPage({Key? key}) : super(key: key);
+
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +12,7 @@ class ChatPage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
-        title: Text('Hi Manaswitha',style: TextStyle(color: Colors.black),),
+        title: Text('Hi Manaswitha'),
         actions: [
           IconButton(onPressed:(){
             print('Icon pressed');
@@ -18,9 +21,28 @@ class ChatPage extends StatelessWidget {
         ],
 
       ),
-      body: Container(),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+                itemCount: 10,
+                itemBuilder: (context, index){
+              return ChatBubble(
+                  alignment: index % 2==0 ?Alignment.centerLeft
+                      : Alignment.centerRight, message:"Hello this is Mana");
+            }),
+            // child: ListView(
+            //   children: [
+            //     ChatBubble(alignment:Alignment.centerLeft, message:"Hello this is Manaswitha"),
+            //     ChatBubble(alignment:Alignment.centerRight, message:"Hi")
+            //   ],
+            // ),
+          ),
+          ChatInput(),
+        ],
+      ),
 
-    );
+      );
 
   }
 }
--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -9,31 +10,31 @@ class LoginPage extends StatelessWidget {
       title: 'Flutter Chat App',
       theme: ThemeData(primarySwatch: Colors.deepOrange),
       home: Scaffold(
-        appBar: AppBar(),
-        drawer: Drawer(),
-        floatingActionButton: FloatingActionButton(
-          onPressed: (){
-            print('Button Clicked');
-          },
-        ),
 
-    body: Row(
-      children: [
-       Text('Let\'s sign you in',style: TextStyle(
-        fontSize: 28,
-        color: Colors.blue,
-        fontWeight: FontWeight.bold,
-        letterSpacing: 0.5
-    ),
-    ),
-        Text(
-        'Welcome back \n You\'ve been missed',
-        style: TextStyle(
-                      fontWeight: FontWeight.bold, fontSize: 20, color: Colors.cyan
-        ),
-        ),
-        Image.network('C:\Users\Manaswitha Sunkara\Desktop\random images\d651f5431ef2c31b8c5e74f39df497ad.jpg', height: 100, width: 100,)
-      ],
+    body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+         Text('Let\'s sign you in',
+           textAlign: TextAlign.center,
+           style: TextStyle(
+          fontSize: 28,
+          color: Colors.blue,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 0.5
+      ),
+      ),
+          Text(
+          'Welcome back \n You\'ve been missed',
+          style: TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: 20, color: Colors.cyan
+          ),
+          ),
+          Image.network('https://cdn.pixabay.com/photo/2017/02/09/21/08/wings-2053515__340.png'),
+
+        ],
+      ),
     ),
     ),
     );
